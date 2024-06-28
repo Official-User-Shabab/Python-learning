fc9155a9a6f451893099414b63324a6394547856
Official-User-Shabab <officialshabab24@gmail.com>
2024-06-28 22:17:19 +0100

    Create Comments.py

---
+++ Comments.py
@@ -0,0 +1,11 @@
+# These are comments (just use "#")
+
+# However if there are multiple lines
+# like this
+# then instead of manually adding "#" each time
+# just highlight/select your multiple lines
+# and press Ctrl + /      (for windows)
+
+# And
+# pretend Ctrl + / was used
+# here
