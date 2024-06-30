c36b8625490d0908fa8f189016fec0a314fc14e4
Official-User-Shabab <officialshabab24@gmail.com>
2024-06-30 11:59:06 +0100

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,14 @@
+x = 8 # simple assignment using "="
+name = "Python" # can be any data types [string, integer, float etc.]
+x = 13 # variables are easily changed too, but wil stay same if no new assignment is done further along the code
+
+age = str(3) # these functions such as str(), int(), and float() are used to convert from one data type to another
+
+# Capital letters are different from lowercase letters
+me = "not you"
+Me2 = "not YOU" # clearly these are different variables
+mE = 'still not you' # strings can be just one speech mark too! (not preferred)
+
+# DO NOT START A VARIABLE NAME WITH A NUMBER OR SYMBOL
+# DO NOT USE A PYTHON KEYWORD LIKE "print"
+# ONLY USE ALPHANUMERIC CHARACHTERS AND UNDERSCORES IN YOUR VARIABLE NAME
