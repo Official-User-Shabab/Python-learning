b432c6cefe855ad79f00fbdef8bc557a8ff19936
Official-User-Shabab <officialshabab24@gmail.com>
2024-07-01 04:43:16 +0100

    Create Data Types.py

---
+++ Data Types.py
@@ -0,0 +1,11 @@
+# str means string
+# int means integer
+# float means float
+# bool is boolean, expressed as True, or False
+# these will be the most common ones you would be using
+
+name = str(input("What's your name?_")) # note that input (taken in) is by default always a string
+age = int(input("How old are you?_")) # so here the integer function was used to convert the string to integer (casting)
+
+x = 6
+print(type(x)) # this function outputs the data type of a variable
