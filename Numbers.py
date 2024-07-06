f6e6d5540a0ef3d1176f734f1c0f128d580735ea
Official-User-Shabab <officialshabab24@gmail.com>
2024-07-07 00:07:33 +0100

    Update Numbers.py

--- Numbers.py
+++ Numbers.py
@@ -1,1 +1,14 @@
+# There are three forms of numeric values: integers, decimals (called a float), complex numbers
 
+a = 16
+b = -32 # Both of these are integers
+
+c = 3.14
+d = -5.6 # Both of these are decimals (called floats)
+
+e = 3j
+f = -7 + 8j # These are complexes (using imaginary numbers) - always using 'j'
+
+# Any type can be converted into another type here, but complex numbers cannot be casted into anything
+# int <-> float
+# float/int -> complex
