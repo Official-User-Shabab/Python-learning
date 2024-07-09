3d513e89e02e7a63af6e8334d86536b0ab5f3d3b
Official-User-Shabab <officialshabab24@gmail.com>
2024-07-09 13:46:55 +0100

    Update Booleans.py

--- Booleans.py
+++ Booleans.py
@@ -1,1 +1,12 @@
+# Values are either True of False in python
+# Any empty list, dictionary and the number 0 are False
+# Otherwise everything else with some content is pretty much True
 
+# This can be used in validation and checking stuff
+
+age = 1
+
+if age == 1:
+  print(f"You are {age} years old") # These are f strings
+
+# Nothing will be printed if the age was not 1
