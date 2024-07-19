d51a25e73cc2e3391935556009b2438905e5cf63
Official-User-Shabab <officialshabab24@gmail.com>
2024-07-19 22:10:38 +0100

    Update 8.1) Arrays.py

--- 8.1) Arrays.py
+++ 8.1) Arrays.py
@@ -1,1 +1,25 @@
+# Arrays store many values in a single variable
 
+fruits = ["banana", "apple", "pear"]
+
+# They are basically the same as lists, and some are imported from libraries
+
+print(fruits[0]) # This prints out the first value of the list/array named fruits
+
+length = len(fruits) # len() is the function to find length of an array
+fruit.append("orange") # .append() method adds a value to the end of array
+fruit.pop(0) # .pop() method removes a value, here the first value, 0, or banana
+fruit.remove("pear") # the .remove() method removes too, and is more specific
+
+# A list of methods for arrays:
+# append()	Adds an element at the end of the list
+# clear()	Removes all the elements from the list
+# copy()	Returns a copy of the list
+# count()	Returns the number of elements with the specified value
+# extend()	Add the elements of a list (or any iterable), to the end of the current list
+# index()	Returns the index of the first element with the specified value
+# insert()	Adds an element at the specified position
+# pop()	Removes the element at the specified position
+# remove()	Removes the first item with the specified value
+# reverse()	Reverses the order of the list
+# sort()	Sorts the list
