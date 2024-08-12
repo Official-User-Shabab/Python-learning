401c1cc6976ba78e132288b24b3a77602b02ba32
Official-User-Shabab <officialshabab24@gmail.com>
2024-08-12 17:07:00 +0100

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -1,1 +1,14 @@
-# How to make functions
+# A function is always DEFINED first, like this:
+def myfunction(num1, num2):
+  print('''
+  The paranthese or "()" include variables, now being called parameters
+  These variables can now be used in the function
+  The function is like a mini codebase within your code
+  Use functions to make your code less repeated and more readable
+  ''')
+  return num1 + num2
+
+
+num1 = 4
+num2 = 8
+sum = myfunction(num1, num2) # These are arguments; variables being sent to or passed into a function
