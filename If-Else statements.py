9e316b5eee43fa6048c08f459247bfbd221c7085
Official-User-Shabab <officialshabab24@gmail.com>
2024-09-27 19:17:22 +0100

    Update If-Else statements.py

--- If-Else statements.py
+++ If-Else statements.py
@@ -7,3 +7,15 @@ else:
   access = False
 
 # note that this is not how most systems handle passwords
+
+# elif is short for else if
+
+num = 5
+studentsAnswer = int(input("Guess the number_"))
+
+if num == 5:
+  print("You got it!")
+elif num == 4 or 6:
+  print("You were 1 away from the answer!")
+else:
+  print("You wrong.")
