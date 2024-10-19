f514df33b9cf6b300997caa28b9f60cfd01579a6
Official-User-Shabab <officialshabab24@gmail.com>
2024-10-19 21:51:43 +0100

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -6,9 +6,11 @@ def myfunction(num1, num2):
   The function is like a mini codebase within your code
   Use functions to make your code less repeated and more readable
   ''')
-  return num1 + num2
+  return num1 + num2 # Returns this back to store in that variable "sum"
 
 
 num1 = 4
 num2 = 8
 sum = myfunction(num1, num2) # These are arguments; variables being sent to or passed into a function
+
+# Think of functions like machines, that can take "boxes" or variables as arguments, and process them
